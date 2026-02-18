965240b1443b516a35c54a16c3d460f09dc86733
jamie <rmk1075@gmail.com>
2026-02-19 00:43:37 +0900

    feat: modify main.py - add requests example code

--- example/main.py
+++ example/main.py
@@ -1,5 +1,11 @@
+import requests
+
+# 대상 URL
+url = 'https://www.google.com'
+
 def main():
-    print("Hello from example!")
+    response = requests.get(url)
+    print(f"<response status_code={response.status_code} text={response.text}>")
 
 
 if __name__ == "__main__":
